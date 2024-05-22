be784143e6b4913065c11679c60f1e5b4cbc9fd1
mixei1902 <misei190289@gmail.com>
2024-05-22 20:12:32 +0300

    lessons 20/2

--- dogs/templatetags/my_tags.py
+++ dogs/templatetags/my_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.filter()
-def media_filtr(path):
+def media_filter(path):
     if path:
-        return f"media/{path}"
+        return f"/media/{path}"
     return '#'
