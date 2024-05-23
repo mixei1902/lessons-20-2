4ec3e2496d2bc708ecfffb4e37df367415f71b36
mixei1902 <misei190289@gmail.com>
2024-05-23 13:16:08 +0300

    lessons 20/2

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from dogs.models import Dog,Breed
+
+from dogs.models import Dog, Breed
 
 
 @admin.register(Dog)
 class DogAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'breed')
+    list_display = ('id', 'name', 'breed')
     list_filter = ('breed',)
     search_fields = ('name',)
 
 
 @admin.register(Breed)
 class BreedAdmin(admin.ModelAdmin):
-    list_display = ('id','name', )
+    list_display = ('id', 'name',)
